a7891c49110744bc947839d25cd9227ad90cb4a3
Martin Rue <hello@martinrue.com>
2020-02-09 19:16:02 +0000

    Add first episode introducing the project

--- js/audio.js
+++ js/audio.js
@@ -2,18 +2,11 @@ const audio = (() => {
   const getFiles = () => {
     return [
       {
-        id: "2",
-        url: "/audio/2.mp3",
-        title: "02: La Dua",
-        desc: "Kaj tiu ĉi, nesurprize, estas la dua ekzempla sondosiero",
-        time: "01:00"
-      },
-      {
         id: "1",
         url: "/audio/1.mp3",
-        title: "01: La Unua",
-        desc: "Tiu ĉi nur estas la unua ekzempla sondosiero",
-        time: "00:06"
+        title: "01: Saluton",
+        desc: "En tiu ĉi unua alŝuto mi prezentas la projekton kaj parolas pri kial mi ĝin kreis.",
+        time: "01:42"
       }
     ];
   };
